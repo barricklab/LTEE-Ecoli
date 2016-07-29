1eb0bec466806c468c3af0189656a3aade794945
jeffreybarrick <jeffrey.e.barrick@gmail.com>
2016-07-29 11:02:23 -0500

    Added additional Ara-3 genomes, pending looking at CNV, these look good

--- LTEE-clone-curated/Ara-3_2000gen_1166A.gd
+++ LTEE-clone-curated/Ara-3_2000gen_1166A.gd
@@ -1,11 +1,10 @@
 #=GENOME_DIFF	1.0
 #=TITLE	Ara-3_2000gen_1166A
-#=AUTHOR	Barrick
 #=TIME	2000
 #=POPULATION	Ara-3
 #=TREATMENT	LTEE
 #=CLONE	A
-#=REFSEQ	BarrickLab-Public:release/reference/REL606.6.gbk
+#=REFSEQ	https://raw.githubusercontent.com/barricklab/LTEE/7da91974eafac0c5a8f903ae57275795d4395af2/reference/REL606.gbk
 #=READSEQ	ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/009/SRR2584859/SRR2584859_1.fastq.gz
 #=READSEQ	ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/009/SRR2584859/SRR2584859_2.fastq.gz
 SNP	1	.	REL606	648692	T

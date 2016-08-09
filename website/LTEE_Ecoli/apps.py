08d971c7dd57d403e7131a706007239898dfe8bc
jeffreybarrick <jeffrey.e.barrick@gmail.com>
2016-08-08 22:06:43 -0500

    Removed django code

--- website/LTEE_Ecoli/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class LteeEcoliConfig(AppConfig):
-    name = 'LTEE_Ecoli'
--- website/LTEE_Ecoli/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class SearchForm(forms.Form):
-    gene_name = forms.CharField(label='Gene name', max_length=100)

e9b20b10f88359d7a05cc2b950e355bb60a6b9cf
Jeffrey Barrick <jeffrey.e.barrick@gmail.com>
2022-08-06 08:43:33 -0500

    Removed populations samples with missing SRA reads

--- LTEE-population/Ara+1_7000gen_REL2525_Good2017.gd
+++
@@ -1,8 +0,0 @@
-#=GENOME_DIFF	1.0
-#=TITLE	Ara+1_7000gen_REL2525
-#=TIME	7000
-#=POPULATION	Ara+1
-#=TREATMENT	LTEE
-#=STUDY_DOI	10.1038/nature24287
-#=REFSEQ	https://raw.githubusercontent.com/barricklab/LTEE/7da91974eafac0c5a8f903ae57275795d4395af2/reference/REL606.gbk
-#=READSEQ	NA
--- LTEE-population/Ara-2_58000gen_REL11626_Good2017.gd
+++
@@ -1,8 +0,0 @@
-#=GENOME_DIFF	1.0
-#=TITLE	Ara-2_58000gen_REL11626
-#=TIME	58000
-#=POPULATION	Ara-2
-#=TREATMENT	LTEE
-#=STUDY_DOI	10.1038/nature24287
-#=REFSEQ	https://raw.githubusercontent.com/barricklab/LTEE/7da91974eafac0c5a8f903ae57275795d4395af2/reference/REL606.gbk
-#=READSEQ	NA

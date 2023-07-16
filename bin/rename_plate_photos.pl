61ba647a4b3b5a693816f5c2dec36159ba8ebd48
Jeffrey Barrick <jeffrey.e.barrick@gmail.com>
2023-07-16 08:05:17 -0500

    transitional rename plate photos

--- bin/rename_plate_photos.pl
+++ bin/rename_plate_photos.pl
@@ -10,29 +10,49 @@ rename_plate_photos.pl
 
 =head1 SYNOPSIS
 
-Usage: rename_plate_photos.pl "[command]"
-
-
+Usage: rename_plate_photos.pl -i raw -o renamed -p "075500gen_#s_#p_24h_bottom_#i"
 
 =head1 DESCRIPTION
 
 =over
 
-=item B<-i,--input> <path to folder> 
+=item B<-i,--input> <path to folder>
 
 Path to input folder.
 
-=item B<-2,--output> <path to folder>
+=item B<-o,--output> <path to folder>
 
-Path to output folder.
+Path to output folder. Created if it doesn't exist.
 
-=item B<-1,--prefix> <string> 
+=item B<-f,--format> <format_string>
 
-Prefix to add to all filenames.
+Format string to create output file names. 
+
+#p will be replaced with the plate type
+#s will be replaced with the sample name
+#i will be replaced with the input file name with the input file name
+
+Default: "#s-#p-#i"
 
 =item B<-2,--suffix> <string>
 
 Suffix to add to all files.
+
+=item B<-s,--samples> <string>
+
+Names of samples or keyword for preset.
+Valid presets:  LTEE-INTERSPERSED, LTEE-INTERSPERSED+ANCESTORS, LTEE-ORDERED, LTEE-ORDERED+ANCESTORS.
+
+Interspersed: A–1, A+1, A–2, A+2, A–3, A+3, A–4, A+4, A–5, A+5, A–6, A+6 
+Ordered:      A–1, A–2, A–3, A–4, A–5, A–6, A+1, A+2, A+3, A+4, A+5, A+6
+
+Int+Anc: REL606, REL607, A–1, A+1, A–2, A+2, A–3, A+3, A–4, A+4, A–5, A+5, A–6, A+6 
+Ord+Anc: REL606, A–1, A–2, A–3, A–4, A–5, A–6, REL607, A+1, A+2, A+3, A+4, A+5, A+6
+
+
+=item B<-p,--plates]> <string>
+
+List of types of plates. Default = [MG, MA, TA]
 
 =back
 
@@ -42,7 +62,7 @@ Jeffrey Barrick
 
 =head1 COPYRIGHT
 
-Copyright 2022.  All rights reserved.
+Copyright 2023.  All rights reserved.
 
 =cut
 
@@ -61,62 +81,123 @@ use File::Path qw(make_path);
 use Getopt::Long;
 use Pod::Usage;
 my ($help, $man);
-my ($input, $output, $prefix, $suffix);
+my ($input, $output, $format);
 my @samples = ();
 my @plates = ();
 
 #pod2usage(1) if (scalar @ARGV == 0);
 GetOptions(
-	'help|?' => \$help, 'man' => \$man,
+	'help|h' => \$help, 'man' => \$man,
 	'input|i=s' => \$input,
 	'output|o=s' => \$output,
-	'prefix|1=s' => \$prefix,
-	'suffix|2=s' => \$suffix,
+	'format|f=s' => \$prefix,
+	'samples|s=s' => \@samples,
+	'plates|p=s' => \@plates
+
 ) or pod2usage(2);
 pod2usage(1) if $help;
 pod2usage(-exitstatus => 0, -verbose => 2) if $man;
 
-$prefix = "" if (!defined $prefix);
+if (scalar (@samples) == 0) {
+	print("ERROR:-s|--sample option required\n\n");
+	pod2usage(-verbose => 1);
+}
+
+
+$prefix = "#p_#s_#i" if (!defined $format);
 $suffix = "" if (!defined $suffix);
 $input = "." if (!defined $input);
 $output = "../output" if (!defined $output);
 
+if (scalar @samples == 1) {
 
-if ( scalar(@samples) == 0 ) {
-	print "Here\n";
-	@samples = (
-		"REL606",
-		"Ara-1",
-		"Ara-2",
-		"Ara-3",
-		"Ara-4",
-		"Ara-5",
-		"Ara-6",
-		"REL607",
-		"Ara+1",
-		"Ara+2",
-		"Ara+3",
-		"Ara+4",
-		"Ara+5",
-		"Ara+6"
-	);
+	my $preset_sample_set_name = "\U$samples[0]";
 
+	if ($preset_sample_set_name eq "LTEE-INTERSPERSED") {
+		@samples = (
+			"Ara-1",
+			"Ara+1",
+			"Ara-2",
+			"Ara+2",
+			"Ara-3",
+			"Ara+3",
+			"Ara-4",
+			"Ara+4",
+			"Ara-5",
+			"Ara+5",
+			"Ara-6",
+			"Ara+6",
+		);
+	} if ($preset_sample_set_name eq "LTEE-INTERSPERSED+ANCESTORS") {
+		@samples = (
+			"Ara-1",
+			"Ara+1",
+			"Ara-2",
+			"Ara+2",
+			"Ara-3",
+			"Ara+3",
+			"Ara-4",
+			"Ara+4",
+			"Ara-5",
+			"Ara+5",
+			"Ara-6",
+			"Ara+6",
+		);
+	} elsif ($preset_sample_set_name eq "LTEE-ORDERED") {
+		@samples = (
+			"Ara-1",
+			"Ara-2",
+			"Ara-3",
+			"Ara-4",
+			"Ara-5",
+			"Ara-6",
+			"Ara+1",
+			"Ara+2",
+			"Ara+3",
+			"Ara+4",
+			"Ara+5",
+			"Ara+6"
+		);
+	}
+	elsif ($preset_sample_set_name eq "LTEE-ORDERED+ANCESTORS") {
+		@samples = (
+			"REL606",
+			"Ara-1",
+			"Ara-2",
+			"Ara-3",
+			"Ara-4",
+			"Ara-5",
+			"Ara-6",
+			"REL607",
+			"Ara+1",
+			"Ara+2",
+			"Ara+3",
+			"Ara+4",
+			"Ara+5",
+			"Ara+6"
+		);
+	}
 }
 
 if ( scalar (@plates) == 0) {
 
 	@plates = (
-		"TA",
 		"MG",
-		"MA"
+		"MA",
+		"TA"
 	);
 
 }
 
+# All plates for one sample, then next sample...
 my @output_file_name_stubs = ();
 foreach my $sample (@samples) {
+	print("$sample\n");
 	foreach my $plate (@plates) {
-	 push @output_file_name_stubs, $sample . ", " . $plate;
+		my $this_file_name_stub = $format;
+		$this_file_name_stub =~ s/\#s/$sample/ge;
+		$this_file_name_stub =~ s/\#p/$population/ge;
+		push $this_file_name_stub;
 	}
 }
 
@@ -142,16 +223,20 @@ my @input_file_names = readdir DIR;
 
 print "Input File Names:" . join(",", @input_file_names) . "\n";
 
+
+exit "Unequal number of input and output file names." if (scalar @output_file_name_stubs != scalar @input_file_names);
+
+
 my @output_file_names = ();
 my $i=0;
 foreach my $input_file_name (@input_file_names) {
+	my $this_output_file_name = $output_file_name_stubs[$i]
+	$this_output_file_name =~ s/\#i/$input_file_name/ge;
 	push @output_file_names, $prefix . $output_file_name_stubs[$i] . $suffix . $input_file_name;
 	$i++;
 }
 
 print "Output File Names:" . join(",", @output_file_names) . "\n";
-
-exit "Unequal number of input and output file names." if (scalar @output_file_names != scalar @input_file_names);
 
 $i=0;
 make_path($output);
